eab9182399404a48691740b576492a10c412ae68
PENGYUXIONG <pxiong@ualberta.ca>
2018-02-27 21:18:13 -0700

    update

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -2,10 +2,15 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import com.robotium.solo.Solo;
 
 import junit.framework.TestCase;
 
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
@@ -14,5 +19,25 @@ public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 
     public void testStart() throws Exception {
         Activity activity = getActivity();
 
+    }
+
+    public void setup() throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void testAddingTweetToTheList(){
+        solo.assertCurrentActivity("Wrong Activity", LonelyTwitterActivity.class);
+        solo.enterText((EditText) solo.getView(R.id.body), "Test Tweet!");
+        solo.clickOnButton("Save");
+
+        assertFalse(solo.searchText("Test Tweet!"));
+
+        solo.clickOnButton("Clear");
+        assertFalse(solo.searchText("Test Tweet!"));
+    }
+
+    @Override
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -38,6 +38,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
@@ -51,6 +52,15 @@ public class LonelyTwitterActivity extends Activity {
 				saveInFile();
 			}
 		});
+		clearButton.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v){
+				setResult(RESULT_OK);
+				tweetList.clear();
+				adapter.notifyDataSetChanged();
+				saveInFile();
+			}
+		});
+
 	}
 
 	@Override
